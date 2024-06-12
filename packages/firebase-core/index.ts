4a5a3cfd01a79fdcf07506c80b95c4c90dc64b42
sukrut sathaye <sukrutssathaye@gmail.com>
2024-06-12 15:25:30 +0200

    add file to index all exports in one place

---
+++ packages/firebase-core/index.ts
@@ -0,0 +1,9 @@
+import FirebaseService from "./src/FirebaseService";
+import AbstractAuth from "./src/AbstractAuth";
+import AuthService from "./src/AuthService";
+
+module.exports = {
+    FirebaseService,
+    AbstractAuth,
+    AuthService
+}
